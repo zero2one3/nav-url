0dc5ba2a16dfa3aa12ed3396734dafad29888a46
Lpyexplore <710805770@qq.com>
2021-02-02 21:29:26 +0800

    feat: 对页面的点击滚动进行了优化

--- src/use/base.js
+++ src/use/base.js
@@ -24,14 +24,14 @@ export default function baseFunction() {
         let end = el.offsetTop - 80
         let each = start > end ? -1 * Math.abs(start - end) / 20 : Math.abs(start - end) / 20
         let count = 0
-        let timer = setInterval(() => {
+        let scroll = () => {
             if(count < 20) {
                 content.scrollTop += each
                 count ++
-            } else {
-                clearInterval(timer)
             }
-        }, 10) 
+            window.requestAnimationFrame(scroll);
+        }
+        scroll()
     }
 
     return {
